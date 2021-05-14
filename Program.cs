21ea064e555aa984591ea0d7e0120717b68cd5a1
dikoleshubh <dikole.shubh@gmail.com>
2021-05-14 23:17:11 +0530

    Welcome to Binary Tree

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BInaryTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WELCOME TO BINARY TREE");
+        }
+    }
+}
